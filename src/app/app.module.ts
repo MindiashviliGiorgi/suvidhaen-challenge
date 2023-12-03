763317e58d493a4db0076ebc2aa803f5121dd694
Giorgi Mindiashvili <126167738+MindiashviliGiorgi@users.noreply.github.com>
2023-12-03 23:11:37 +0400

    header-done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { PortfolioPageComponent } from './pages/portfolio-page/portfolio-page.component';
+import { TeamsPageComponent } from './pages/teams-page/teams-page.component';
+import { CareersPageComponent } from './pages/careers-page/careers-page.component';
+import { CurrentOpeningsPageComponent } from './pages/current-openings-page/current-openings-page.component';
+import { ContactUsPageComponent } from './pages/contact-us-page/contact-us-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    PortfolioPageComponent,
+    TeamsPageComponent,
+    CareersPageComponent,
+    CurrentOpeningsPageComponent,
+    ContactUsPageComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/current-openings-page/current-openings-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-current-openings-page',
+  templateUrl: './current-openings-page.component.html',
+  styleUrls: ['./current-openings-page.component.scss']
+})
+export class CurrentOpeningsPageComponent {
+
+}
