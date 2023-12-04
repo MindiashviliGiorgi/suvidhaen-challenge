017a22335e03718adc93a7bb8dfdbdb76e43c5ed
Giorgi Mindiashvili <126167738+MindiashviliGiorgi@users.noreply.github.com>
2023-12-04 19:03:51 +0400

    full-home-page-done

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  header_variable : boolean = false;
 
+  constructor() {};
+
+  ngOnInit(): void {};
+
+  @HostListener("document:scroll")
+  scrollFunction() {
+    let pos = document.documentElement.scrollTop;
+    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100) / calcHeight);
+
+    if (scrollValue > 3) {
+      this.header_variable = true;
+    } else {
+      this.header_variable = false;
+    }
+  }
 }
--- src/app/components/welcome-home/welcome-home.component.ts
+++ src/app/components/welcome-home/welcome-home.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./welcome-home.component.scss']
 })
 export class WelcomeHomeComponent {
+  images: string[] = [
+    '/assets/images/welcome-images/img1.svg',
+    '/assets/images/welcome-images/img2.svg',
+    '/assets/images/welcome-images/img3.svg',
+    '/assets/images/welcome-images/img4.svg',
+    '/assets/images/welcome-images/img5.svg',
+    '/assets/images/welcome-images/img6.svg',
+    '/assets/images/welcome-images/img7.svg',
+  ];
+
+  selectedImage : string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.getRandomImage();
+  }
+  
+  getRandomImage() {
+    const randomIndex = Math.floor(Math.random() * this.images.length);
+    this.selectedImage = this.images[randomIndex];
+  }
 
 }
